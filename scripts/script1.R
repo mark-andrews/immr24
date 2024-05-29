4dd9bbd8f98f4b4ce078b57e1d807c6de696d9ed
Mark Andrews <mjandrews.org@gmail.com>
2024-05-29 10:53:57 +0100

    Update script1

--- scripts/script1.R
+++ scripts/script1.R
@@ -7,4 +7,21 @@ rats_df_42 <- filter(rats_df, batch == 42)
 # binomial model, as glm
 M_1 <- glm(cbind(m, n-m) ~ 1, data = rats_df_42, family = binomial())
 
+# MLE estimate of probability of developing tumour in batch 42
 plogis(coef(M_1))
+
+# 95% confidence interval on that probability
+plogis(confint.default(M_1))
+
+
+# binomial model, of all sub-populations, assuming fixed prob of tumour
+# across all sub-populations
+M_2 <- glm(cbind(m, n-m) ~ 1, data = rats_df, family = binomial())
+
+# MLE estimate of probability of tumour
+plogis(coef(M_2))
+
+# 95% confidence interval for this estimate
+plogis(confint.default(M_2))
+
+
